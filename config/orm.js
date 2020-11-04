7cc3c02813a70b701e34e13127e7163fb5ea9cba
troylochner <troy.lochner@gmail.com>
2020-11-04 08:24:27 -0500

    udpated configs + models + orm

---
+++ config/orm.js
@@ -0,0 +1,57 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+//ORM SETUP
+var orm = {
+    selectAll: function(tableInput, cb) {
+      var queryString = "SELECT * FROM " + tableInput + ";";
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
+    },
+
+    insertOne: function(tableInput, cols, vals, cb) {
+      var queryString = "INSERT INTO " + tableInput;
+  
+      queryString += " (";
+      queryString += cols.toString();
+      queryString += ") ";
+      queryString += "VALUES (";
+      queryString += printQuestionMarks(vals.length);
+      queryString += ") ";
+  
+      console.log(queryString);
+  
+      connection.query(queryString, vals, function(err, result) {
+        if (err) {
+          throw err;
+        }
+  
+        cb(result);
+      });
+    },
+    
+    updateOne: function(tableInput, objColVals, condition, cb) {
+      var queryString = "UPDATE " + tableInput;
+  
+      queryString += " SET ";
+      queryString += objToSql(objColVals);
+      queryString += " WHERE ";
+      queryString += condition;
+  
+      console.log(queryString);
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+  
+        cb(result);
+      });
+    }
+  };
+
+// Export the orm object for the model
+module.exports = orm;
---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+var mysql = require("mysql");
+
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
