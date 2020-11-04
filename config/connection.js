66481f4abf81b38281ae57df9a3031e316234cf0
troylochner <troy.lochner@gmail.com>
2020-11-04 08:55:04 -0500

    MVC Setup - need to adjust the handlebars views - those aren't function properly

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection({
   port: 3306,
   user: "test_user",
   password: "testPW",
-  database: "burger_db"
+  database: "burgers_db"
 });
 
 // Make connection.
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,9 +7,9 @@ var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  burger.all(function(data) {
+  burger.selectAll(function(data) {
     var hbsObject = {
-      cats: data
+      burgers: data
     };
     console.log(hbsObject);
     res.render("index", hbsObject);
@@ -17,7 +17,7 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burger", function(req, res) {
-  burger.create(["burger_name"], [req.body.name], function(result) {
+  burger.insertOne(["burger_name"], [req.body.name], function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
@@ -28,7 +28,7 @@ router.put("/api/burger/:id", function(req, res) {
 
   console.log("condition", condition);
 
-  burger.update(
+  burger.updateOne(
     {
         devoured: req.body.devoured
     },
